d0e637a4bd45c33d8a23bf6a50d407c469eedf87
Stuart Berg <bergs@janelia.hhmi.org>
2015-04-15 23:06:10 -0400

    Globals: Obtain integer type definitions from boost/cstdint.hpp for portability

--- libdvid/Globals.h
+++ libdvid/Globals.h
@@ -8,23 +8,16 @@
 #ifndef DVIDGLOBALS_H
 #define DVIDGLOBALS_H
 
-#include <boost/static_assert.hpp>
+#include <boost/cstdint.hpp>
 #include <climits>
 
 namespace libdvid {
 
-//! Asssume long long are 64 bits
-//! TODO: make more robust for more build environments
-typedef unsigned long long uint64;
-
-//! Ensure uint64 is 8 bytes
-BOOST_STATIC_ASSERT(sizeof(uint64) == 8);
+typedef boost::uint8_t uint8;
+typedef boost::uint64_t uint64;
 
 //! By default everything in DVID has 32x32x32 blocks
 const int DEFBLOCKSIZE = 32;
-
-//! Grayscale type is 1 byte
-typedef unsigned char uint8;
 
 //! Gives the limit for how many vertice can be operated on in one call
 const int TransactionLimit = 1000;
