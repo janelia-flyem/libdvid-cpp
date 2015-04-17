2e4ae48c9b5c1a4c7c9fb65b9d227473ec905726
Stuart Berg <bergs@janelia.hhmi.org>
2015-04-17 16:34:44 -0400

    python: tests: better instance names.

--- python/tests/test_node_service.py
+++ python/tests/test_node_service.py
@@ -24,28 +24,28 @@ class Test_DVIDNodeService(unittest.TestCase):
 
     def test_keyvalue(self):
         node_service = DVIDNodeService(TEST_DVID_SERVER, self.uuid)
-        node_service.create_keyvalue("stuart_keyvalue")
-        node_service.put("stuart_keyvalue", "kkkk", "vvvv")
-        readback_value = node_service.get("stuart_keyvalue", "kkkk")
+        node_service.create_keyvalue("test_keyvalue")
+        node_service.put("test_keyvalue", "kkkk", "vvvv")
+        readback_value = node_service.get("test_keyvalue", "kkkk")
         self.assertEqual(readback_value, "vvvv")
  
         with self.assertRaises(RuntimeError):
-            node_service.put("stuart_keyvalue", "kkkk", 123) # 123 is not a buffer.
+            node_service.put("test_keyvalue", "kkkk", 123) # 123 is not a buffer.
 
-    def test_grayscale8(self):
+    def test_grayscale_3d(self):
         node_service = DVIDNodeService(TEST_DVID_SERVER, self.uuid)
-        node_service.create_grayscale8("stuart_grayscale")
+        node_service.create_grayscale8("test_grayscale_3d")
         data = numpy.random.randint(0, 255, (128,128,128)).astype(numpy.uint8)
-        node_service.put_gray3D( "stuart_grayscale", data, (0,0,0) )
-        retrieved_data = node_service.get_gray3D( "stuart_grayscale", (30,30,30), (20,20,20) )
+        node_service.put_gray3D( "test_grayscale_3d", data, (0,0,0) )
+        retrieved_data = node_service.get_gray3D( "test_grayscale_3d", (30,30,30), (20,20,20) )
         self.assertTrue( (retrieved_data == data[20:50, 20:50, 20:50]).all() )
 
-    def test_labels64(self):
+    def test_labels_3d(self):
         node_service = DVIDNodeService(TEST_DVID_SERVER, self.uuid)
-        node_service.create_labelblk("stuart_labelblk")
+        node_service.create_labelblk("test_labels_3d")
         data = numpy.random.randint(0, 2**63-1, (128,128,128)).astype(numpy.uint64)
-        node_service.put_labels3D( "stuart_labelblk", data, (0,0,0) )
-        retrieved_data = node_service.get_labels3D( "stuart_labelblk", (30,30,30), (20,20,20) )
+        node_service.put_labels3D( "test_labels_3d", data, (0,0,0) )
+        retrieved_data = node_service.get_labels3D( "test_labels_3d", (30,30,30), (20,20,20) )
         self.assertTrue( (retrieved_data == data[20:50, 20:50, 20:50]).all() )
 
 if __name__ == "__main__":
