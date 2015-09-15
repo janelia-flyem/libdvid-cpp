20a6f151ee47179a41d211b6c0a1b426c63fe4f5
Stuart Berg <bergs@janelia.hhmi.org>
2015-09-15 19:09:23 -0400

    Replaced compile_against_conda.sh with updated version configure-for-conda.sh

---
+++ configure-for-conda.sh
@@ -0,0 +1,31 @@
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <install-environment-dir>"
+    exit 1;
+fi
+
+# Start in the same directory as this script.
+cd `dirname $0`
+
+export PREFIX="$1"
+export PYTHON="${PREFIX}/bin/python"
+export CPU_COUNT=`python -c "import multiprocessing; print multiprocessing.cpu_count()"`
+export PATH="${PREFIX}/bin":$PATH
+
+# Install gcc first if necessary.
+if [ ! -e "${PREFIX}/bin/gcc" ]; then
+    conda install -p "${PREFIX}" -c flyem gcc
+fi
+
+# Do you already have curl?
+curl --help > /dev/null
+if [[ $? == 0 ]]; then
+    CURL=curl
+else
+    # Install to conda prefix
+    conda install -p "${PREFIX}" curl
+    CURL="${PREFIX}/bin/curl"
+fi
+
+BUILD_SCRIPT_URL=https://raw.githubusercontent.com/janelia-flyem/flyem-build-conda/master/libdvid-cpp/build.sh
+"$CURL" "$BUILD_SCRIPT_URL" | bash -x -e -s - --configure-only
+
